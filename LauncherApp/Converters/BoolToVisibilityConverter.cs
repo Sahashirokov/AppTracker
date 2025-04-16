4579d577e6d98ef4dc78bf6925e53cddc68bfe2b
Александр Широков <sahca.shirokov.69@gmail.com>
2025-04-16 22:33:01 +0500

    добавил условие если нет элементов то выводить что ничего не выбрано

---
+++ LauncherApp/Converters/BoolToVisibilityConverter.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace LauncherApp.Converters;
+
+public class BoolToVisibilityConverter: IValueConverter
+{
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            // Если значение true → Visible, иначе Collapsed
+            return (value is bool && (bool)value) ? Visibility.Visible : Visibility.Collapsed;
+        }
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) => throw new NotImplementedException();
+}
